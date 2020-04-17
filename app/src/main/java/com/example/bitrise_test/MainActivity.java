13760f8139ef7559e18c8ef3fdd4a6e50412a305
chocovayashi <c.tsujiyan727@gmail.com>
2020-04-17 14:20:57 +0900

    reward

--- app/src/main/java/com/example/bitrise_test/MainActivity.java
+++ app/src/main/java/com/example/bitrise_test/MainActivity.java
@@ -2,54 +2,94 @@ package com.example.bitrise_test;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.test.espresso.idling.CountingIdlingResource;
 
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+
+import jp.fluct.fluctsdk.FluctErrorCode;
+import jp.fluct.fluctsdk.FluctRewardedVideo;
+import jp.fluct.fluctsdk.FluctRewardedVideoSettings;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FluctRewardedVideo rewardedVideo;
+    private CountingIdlingResource idlingResource;
+    private boolean isFinished;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        idlingResource = getOrStoreIdlingResource();
+
+        FluctRewardedVideoSettings settings = new FluctRewardedVideoSettings.Builder()
+                .debugMode(true)
+                .build();
+
+        rewardedVideo = FluctRewardedVideo.getInstance("1000117639", "1000199372", this, settings);
+
+        rewardedVideo.setListener(new FluctRewardedVideo.Listener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onLoaded(String groupId, String unitId) {
+                Log.d("MainActivity", "onLoaded");
+                rewardedVideo.show();
+            }
+
+            @Override
+            public void onFailedToLoad(String groupId, String unitId, FluctErrorCode errorCode) {
+                Log.d("MainActivity", "onFailedToLoad");
+            }
+
+            @Override
+            public void onOpened(String groupId, String unitId) {
+                Log.d("MainActivity", "onOpened");
+            }
+
+            @Override
+            public void onStarted(String groupId, String unitId) {
+                Log.d("MainActivity", "onStarted");
+            }
+
+            @Override
+            public void onShouldReward(String groupId, String unitId) {
+                Log.d("MainActivity", "onShouldReward");
+                isFinished = true;
+                getOrStoreIdlingResource().decrement();
+            }
+
+            @Override
+            public void onClosed(String groupId, String unitId) {
+                Log.d("MainActivity", "onClosed");
+            }
+
+            @Override
+            public void onFailedToPlay(String groupId, String unitId, FluctErrorCode errorCode) {
+                Log.d("MainActivity", "onFailedToPlay");
+            }
+        });
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                idlingResource.increment();
+                rewardedVideo.loadAd();
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        public CountingIdlingResource getOrStoreIdlingResource() {
+            if (idlingResource == null) {
+                idlingResource = new CountingIdlingResource("main");
+            }
+            return idlingResource;
         }
 
-        return super.onOptionsItemSelected(item);
-    }
+        public boolean isFinished() {
+            return isFinished;
+        }
 }
